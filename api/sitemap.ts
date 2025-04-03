caeb5326d7725696aaf6edaeb467ab85776518b9
Geun-Hwan <rmsghks1026@gmail.com>
2025-04-03 17:35:14 +0900

    fix : 네이버 사이트맵 추가

--- api/sitemap.ts
+++ api/sitemap.ts
@@ -20,18 +20,35 @@ export default async function handler(req: any, res: any) {
   // 여기서 동적으로 가져올 데이터 (예: DB에서 questionId 리스트 가져오기)
   const publicIds = await fetchPublicIds();
 
+  const importantRoutes = ["/", "/balance", "/predict"];
+
   const staticRoutes = [
-    "/",
     "/login",
     "/join",
     "/contact",
     "/setting",
-    "/balance",
-    "/predict",
     "/mypage",
     "/my-games",
     "/my-games#my-participations",
   ];
+
+  const importantUrls = importantRoutes.map(
+    (path) => `
+    <url>
+      <loc>${siteUrl}${path}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <priority>1.0</priority>
+    </url>`
+  );
+
+  const staticUrls = staticRoutes.map(
+    (path) => `
+    <url>
+      <loc>${siteUrl}${path}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <priority>0.8</priority>
+    </url>`
+  );
 
   // 동적 페이지 URL 생성 (balance/public/:questionId)
   const dynamicUrls = publicIds.map(
@@ -46,15 +63,8 @@ export default async function handler(req: any, res: any) {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
     ${[
-      ...staticRoutes.map(
-        (path) => `
-        <url>
-          <loc>${siteUrl}${path}</loc>
-          <lastmod>${new Date().toISOString()}</lastmod>
-          <priority>0.8</priority>
-        </url>
-      `
-      ),
+      ...importantUrls,
+      ...staticUrls,
       ...dynamicUrls, // 이제 문자열이 아니라 배열이므로 올바르게 들어감
     ].join("")}
   </urlset>`;
